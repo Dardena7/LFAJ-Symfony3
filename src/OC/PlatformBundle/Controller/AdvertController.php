e8aa943377f313013f143083c1c253a954b82a91
Alexis <alexis.dardenne7@gmail.com>
2018-09-23 23:18:47 +0200

    Initial commit + first page and bundle creation

---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class AdvertController extends Controller
+{
+  public function indexAction()
+  {
+    return $this->render('@OCPlatform/Advert/index.html.twig');
+  }
+}
+
+?>
