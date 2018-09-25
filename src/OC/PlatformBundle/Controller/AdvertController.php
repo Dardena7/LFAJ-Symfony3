f9dc0b8559f14ffd4718b53f08c36d13fdcd262d
Alexis <alexis.dardenne7@gmail.com>
2018-09-25 15:24:00 +0200

    Chapter 10, 11 entities + manipulation with Doctrine2

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -3,9 +3,11 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use OC\PlatformBundle\Entity\Advert;
 
 
 class AdvertController extends Controller
@@ -40,28 +42,43 @@ class AdvertController extends Controller
     //return $response;
     //return new JsonResponse(array('id' => $id));
     //return $this->redirectToRoute('oc_platform_homepage', array('page' => $id));
+    $repository = $this->getDoctrine()
+      ->getManager()
+      ->getRepository('OCPlatformBundle:Advert');
+
+    $advert = $repository->find($id);
+
+    if(null === $advert) {
+      throw new NotFoundHttpException("L'annonce avec l'id ".$id." n'existe pas !");
+    }
+
     return $this->render('@OCPlatform/Advert/view.html.twig',
-      array('id' => $id)
+      array('advert' => $advert)
     );
   }
 
   public function addAction(Request $request)
   {
-    //$session = $request->getSession();
-    //$session->getFlashBag()->add('info', 'Annonce bien enregistrée');
-    //$session->getFlashBag()->add('info', 'Confirmation !');
+    $advert = new Advert;
+    $advert->setTitle('Symfony3book');
+    $advert->setAuthor('Alexis');
+    $advert->setContent("Tutoriel sur le framework Symfony version 3");
+
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($advert);
+    $em->flush();
+    $session = $request->getSession();
+
     if($request->isMethod('POST'))
     {
       $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée');
-      return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+      return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
     }
-    $antispam = $this->get('oc_platform.antispam');
-    $text = ".....";
-    if($antispam->isSpam($text))
-      throw new \Exception('Votre message a été détecté comme spam !');
-    return $this->render('@OCPlatform/Advert/add.html.twig',
-      array('text' => $text)
-    );
+    //$antispam = $this->get('oc_platform.antispam');
+    //$text = ".....";
+    //if($antispam->isSpam($text))
+    //  throw new \Exception('Votre message a été détecté comme spam !');
+    return $this->render('@OCPlatform/Advert/add.html.twig');
   }
 
   public function editAction($id, Request $request)
